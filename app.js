d7f3db29cc6089ea56d50771943ec56dae843d35
Michael Hamilton <michaelhamilton626@gmail.com>
2018-02-01 23:26:09 -0500

    Implemented pug, migrated html templates to pug

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(http);
 var path = require('path');
 var bodyParser = require('body-parser');
 
+app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/jquery', express.static(__dirname + '/bower_components/jquery/dist/'));
@@ -13,11 +14,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.render('presentation-finder', {});
 });
 
 app.get('/present', function(req, res) {
-    res.sendFile(__dirname + '/presenter.html');
+    res.render('presenter', {});
 });
 
 app.post('/view', function(req, res) {
@@ -25,7 +26,7 @@ app.post('/view', function(req, res) {
 });
 
 app.get('/view/:id', function(req, res) {
-    res.sendFile(__dirname + '/viewer.html');
+    res.render('viewer', {});
 });
 
 io.on('connection', function(socket) {
